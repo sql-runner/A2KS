4eaf28e0d81ef867c5e60f13400e44f43907829f
a2k11 <adam.kaewell@gmail.com>
2014-12-02 15:58:12 -0500

    A user can upload an avatar to describe who they are as a person and as a user of this website.  I used paperclip to implement this feature. Gitignore now has ignored all of the images paperclip saves.  I updated the stylesheets to only run to the width of a page as well.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
