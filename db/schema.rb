41beb604d455eab747003dbcb9335728acd14eb6
a2k11 <adam.kaewell@gmail.com>
2014-12-02 15:58:12 -0500

    A user can upload an avatar to describe who they are as a person and as a user of this website.  I used paperclip to implement this feature. Gitignore now has ignored all of the images paperclip saves.  I updated the stylesheets to only run to the width of a page as well.

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107205649) do
+ActiveRecord::Schema.define(version: 20141202194437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",               null: false
+    t.string   "password_digest",     null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
