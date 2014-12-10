bb863027bcfeb97ec883ada5b306a9d761985ab9
a2k11 <adam.kaewell@gmail.com>
2014-12-10 15:25:21 -0500

    appease the hound

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,8 +5,8 @@ class Video < ActiveRecord::Base
   do_not_validate_attachment_file_type :media
   belongs_to :user
 
-  WEB_MP4_PRESET_ID="1351620000001-000020"
-  AWS_PIPELINE_ID="1418138948317-hgb52z"
+  WEB_MP4_PRESET_ID = "1351620000001-000020"
+  AWS_PIPELINE_ID = "1418138948317-hgb52z"
 
   def original_file_path
     s3_file_name = media.url.gsub(/.*\/\/.*\/\d+\//, "")
@@ -24,17 +24,17 @@ class Video < ActiveRecord::Base
       pipeline_id: AWS_PIPELINE_ID,
       input: {
         key: the_s3_path,
-        frame_rate: 'auto',
-        resolution: 'auto',
-        aspect_ratio: 'auto',
-        interlaced: 'auto',
-        container: 'auto'
+        frame_rate: "auto",
+        resolution: "auto",
+        aspect_ratio: "auto",
+        interlaced: "auto",
+        container: "auto"
       },
       output: {
         key: the_s3_path + ".mp4",
         preset_id: WEB_MP4_PRESET_ID,
         thumbnail_pattern: "",
-        rotate: '0'
+        rotate: "0"
       }
     )
   end
