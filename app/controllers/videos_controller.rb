9dcc2bc781637cf6e7cf4081e878f4ea83e8c9d9
a2k11 <adam.kaewell@gmail.com>
2014-12-10 17:23:19 -0500

    Fixed Peoples suggestions

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,10 +17,14 @@ class VideosController < ApplicationController
   end
 
   def show
-    @video = Video.find(params[:id])
+    @video = find_video
   end
 
   private
+
+  def find_video
+    Video.find(params[:id])
+  end
 
   def find_video_params
     params.require(:video).permit(
