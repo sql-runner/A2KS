a25fb4c6b9e1ea7f60146ad3895daf84ac6cdc65
a2k11 <adam.kaewell@gmail.com>
2014-12-10 15:16:46 -0500

    Refactoring a little bit

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,17 +1,7 @@
 class Video < ActiveRecord::Base
-  has_attached_file :media,
-    path: "/:class/:id/:filename",
-#    styles: {
-#      medium: { geometry: "640x480", format: "flv",
-#                convert_options: {output: {ar: 44100}}
-#    },
-#      large: { geometry: "1024x576", format: "flv",
-#               convert_options: {output: {ar: 44100}}
-#      }
-#    },
+  has_attached_file :media, path: "/:class/:id/:filename",
     processors: [:transcoder],
     use_timestamp: false
-
   do_not_validate_attachment_file_type :media
   belongs_to :user
 
